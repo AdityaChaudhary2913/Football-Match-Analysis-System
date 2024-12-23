7ec38cba6526819ccca6f194a3f547381c011af2
AdityaTheJaat <adityachaudhary1306@gmail.com>
2024-12-23 15:26:56 +0530

    model training completed

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # Initialize Tracker
-    tracker = Tracker('models/best_youtube.pt')
+    tracker = Tracker('models/best.pt')
 
     tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
     
